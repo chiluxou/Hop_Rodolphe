3a23eb52741c690ac713382cb858305f67360ce0
chiluxou <rodo.couzin@free.fr>
2024-12-14 21:48:09 +0100

    Update Field.java
    change the constructeur and create the method initialiseeBlocks

--- Field.java
+++ Field.java
@@ -1,19 +1,27 @@
 import java.util.ArrayList;
-import java.util.List;    
+import java.util.List;
+import java.util.Random;    
 
 public class Field {
-    public static final int ALTITUDE_GAP = 80;
-    public static final int START_ALTITUDE = 40;
+     // Constants for block generation
+     public static final int ALTITUDE_GAP = 80; // Vertical gap between blocks
+     public static final int START_ALTITUDE = 40; // Initial altitude for the first block
+     public static final int MAX_BLOCK_WIDTH = 100; // Maximum width of a block
+     public static final int MIN_BLOCK_WIDTH = 40;  // Minimum width of a block
 
-    public final int width, height;
-    private int bottom, top; // bottom and top altitude
+    
+
+    public final int height; // Field height
+    public final int width;  // Field width
+    private final List<Block> blocks; // List to store blocks
+    //private int bottom, top; // bottom and top altitude
 
     public Field(int width, int height) {
         this.width = width;
         this.height = height;
+        this.blocks = new ArrayList<>(); // Initialize the list of blocks
+        initializeBlocks(); // Generate blocks when Field is created
     }
-
-    private final List<Block> blocks = new ArrayList<>();
 
     public void update() {
         for (Block block : blocks) {
@@ -24,12 +32,23 @@ public class Field {
     }
 }
 
-public void addBlock(Block block) {
-    blocks.add(block);
-}
+    public void addBlock(Block block) {
+        blocks.add(block);
+    }
 
-public List<Block> getBlocks() {
-    return blocks;
-}
+    private void initializeBlocks() {
+        Random random = new Random();
+
+        // Générer des blocs à intervalles réguliers d'altitude
+        for (int altitude = height - START_ALTITUDE; altitude > 0; altitude -= ALTITUDE_GAP) {
+            int blockWidth = MIN_BLOCK_WIDTH + random.nextInt(MAX_BLOCK_WIDTH - MIN_BLOCK_WIDTH + 1); // Largeur aléatoire
+            int blockX = random.nextInt(width - blockWidth); // Position horizontale aléatoire
+            blocks.add(new Block(blockX, altitude, blockWidth)); // Ajouter le bloc
+        }
+    }
+
+    public List<Block> getBlocks() {
+        return blocks;
+    }
 
 }
