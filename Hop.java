80692e01d4f29529b63b9061f7c6b2304218f243
chiluxou <rodo.couzin@free.fr>
2024-12-15 14:44:53 +0100

    Update Hop.java

--- Hop.java
+++ Hop.java
@@ -1,62 +1,49 @@
-import java.awt.event.ActionEvent;
 import javax.swing.*;
 
 public class Hop {
-    public static final int WIDTH = 400;
-    public static final int HEIGHT = 600;
-    public static final int DELAY = 40;
-
-    private final JFrame frame;
     private final Field field;
     private final Axel axel;
+    private final GamePanel gamePanel;
+    public int score = 0;
     private Timer timer;
-    private GamePanel gamePanel;
 
     public Hop() {
-        this.field = new Field(WIDTH, HEIGHT);
-        Block bottomBlock = field.getBlocks().get(0);
-        this.axel = new Axel(field, bottomBlock.getX() + (bottomBlock.getWidth() / 2), 
-                             bottomBlock.getY() - GamePanel.getAxelHeight());
-        this.gamePanel = new GamePanel(field, axel);
-    
-        this.frame = new JFrame("Hop!");
+        field = new Field(400, 600);
+        Block firstBlock = field.getBlocks().get(0);
+        axel = new Axel(field, firstBlock.getX() + firstBlock.getWidth() / 2, firstBlock.getY() - GamePanel.getAxelHeight());
+
+        gamePanel = new GamePanel(field, axel);
+        JFrame frame = new JFrame("Hop!");
         frame.add(gamePanel);
         frame.pack();
-        frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-    
-    
+        frame.setVisible(true);
 
-    public boolean over() {
-        return false;
-    }
-
-    public static void main(String[] args) {
-        Hop game = new Hop();
-
-        game.timer = new Timer(DELAY, (ActionEvent e) -> {
-            game.round();
-            if (game.over()) {
-                game.timer.stop();
-                game.frame.remove(game.gamePanel);
-            }
-        });
-        game.timer.start();
+        Timer timer = new Timer(16, e -> round());
+        timer.start();
     }
 
     public void round() {
-        axel.update();
-        field.update();
-
-        // Vérifier les collisions
-        for (Block block : field.getBlocks()) {
-            if (block.collidesWith(axel.getX(), axel.getY(), GamePanel.getAxelWidth(), GamePanel.getAxelHeight())) {
-                axel.setSurviving(false); // Collision détectée
-            }
+        axel.update(); // Mettre à jour Axel (déplacement, saut, gravité)
+        field.update(); // Mettre à jour les blocs (défilement et génération)
+    
+        // Vérifier si le jeu est terminé
+        if (over()) {
+            timer.stop();
+            System.out.println("Game Over! Score final : " + score);
         }
+    
+        gamePanel.repaint(); // Rafraîchir l'affichage
+        score++;
+    }
+    
 
-        frame.repaint();
+    public static void main(String[] args) {
+        new Hop();
     }
 
+    public boolean over() {
+        return !axel.isSurviving(); // Retourne true si Axel n'est plus en vie
+    }
 }
+
